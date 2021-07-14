fd26fa947e6819de01e88a28b1d06e07a11b9823
gitct <choh1997@naver.om>
2021-07-14 10:00:53 +0900

    07.14 - 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -7,6 +8,12 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('Create/',AccountCreateView.as_view, name='Create')
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+
+    path('logout/', LoginView.as_view(), name='logout'),
+
+
+    path('create/', AccountCreateView.as_view(), name='create')
 
 ]
