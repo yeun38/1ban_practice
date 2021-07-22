871615e2616d960fa25c5c3a511b0f5e5297c2c0
gitct <choh1997@naver.om>
2021-07-22 15:52:42 +0900

    07.22 - 3

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,15 @@
+from email.mime import image
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+    # user라는 객체가 살아졌을때 cascade(종속)도 사라짐.
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True) # CharField > 텍스트
+    message = models.CharField(max_length=200, null=True)
