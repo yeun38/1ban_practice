afc7f1295713044ada2f506ad143959e872976ba
gitct <choh1997@naver.om>
2021-07-28 10:49:34 +0900

    07.28 - 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -36,8 +36,11 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.pk})
 
 class AccountDetailView(DetailView):
     model = User
@@ -52,8 +55,11 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:detail')
+    # success_url = reverse_lazy('accountapp:detail')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.pk})
 
 
 
