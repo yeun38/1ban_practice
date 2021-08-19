4ae5b87539ae6578808adcfa0bbf763d4e6f7abe
gitct <choh1997@naver.om>
2021-08-19 15:44:05 +0900

    08.19 - 04

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
 
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
 
 urlpatterns = [
+
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
 
 ]
