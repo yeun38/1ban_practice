d8bbec7a7d476efb6c358fa382b7e27e86d9aa9e
gitct <choh1997@naver.om>
2021-09-02 23:43:15 +0900

    09.02-02

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,26 +16,6 @@ from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 
-@login_required(login_url=reverse_lazy('accountapp:login'))
-def hello_world(request):
-    if request.method == 'POST':
-
-        temp = request.POST.get("hello_world_input")
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        # return HttpResponseRedirect(reverse('accountapp:hello'))
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world2.html',
-                      context={'hello_world_list': hello_world_list})
-
-    else :
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world2.html',
-                      context={'hello_world_list': hello_world_list})
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
@@ -78,7 +58,7 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView) :
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
 
--- djangoProject_practice_01/urls.py
+++ djangoProject_practice_01/urls.py
@@ -18,9 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
+from articleapp.views import ArticleListView
 
 urlpatterns = [
+    path('', ArticleListView.as_view(), name='home'),
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
@@ -30,4 +32,5 @@ urlpatterns = [
 
     path('subscribe/', include('subscribeapp.urls')),
     path('likes/', include('likeapp.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
